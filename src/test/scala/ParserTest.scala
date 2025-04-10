2766adb59d71c20d8d2c6bed6dedc1c22eb7d95c
James Lee-Jones <james@lee-jones.co.uk>
2025-04-10 19:22:47 +0100

    refactor: refactor regex paring to lex then parse
    Refactor the regex parser to first convert a regular expression into a
    list of tokens using a lexer. The list of tokens is then parsed using a
    seperate parser to an internal representation of a regular expression.
    This improves the seperation between the string representation and the
    NFA that will be built from the regex.

---
+++ src/test/scala/ParserTest.scala
@@ -0,0 +1,76 @@
+class ParserTest
+
+import lexer.{Alt, Ch, Emp, Parser, RegEx, Scanner, Sequence, Star}
+import org.scalatest.flatspec.AnyFlatSpec
+
+
+class ParserFlatSpec extends AnyFlatSpec {
+  def parsePrintInverse(regex: String): Unit = {
+    val regex = Parser.parse(Scanner.scan("(a|b)*"))
+    val printed = RegEx.prettyPrint(regex)
+    val reparsedRegex = Parser.parse(Scanner.scan(printed))
+    assert(regex == reparsedRegex)
+  }
+
+  "An empty regex" should "return Emp" in {
+    assert(Parser.parse(Scanner.scan("")) == Emp)
+  }
+
+  "An constant regex" should "return a constant" in {
+    assert(Parser.parse(Scanner.scan("a")) == Ch('a'))
+    assert(Parser.parse(Scanner.scan("4")) == Ch('4'))
+  }
+
+  "An sequence regex" should "return a Seq" in {
+    assert(Parser.parse(Scanner.scan("gb")) == Sequence(Ch('g'), Ch('b')))
+    assert(Parser.parse(Scanner.scan("gb64")) == Sequence(Ch('g'), Ch('b'), Ch('6'), Ch('4')))
+  }
+
+  "An alternation" should "return an alternation with the regexes either side" in {
+    assert(Parser.parse(Scanner.scan("a|b")) == Alt(Ch('a'), Ch('b')))
+    assert(Parser.parse(Scanner.scan("a1|b2")) == Alt(Sequence(Ch('a'), Ch('1')),
+                                                      Sequence(Ch('b'), Ch('2'))))
+  }
+
+  "Matching brackets" should "parse correctly" in {
+    assert(Parser.parse(Scanner.scan("()")) == Emp)
+    assert(Parser.parse(Scanner.scan("(a)")) == Ch('a'))
+    assert(Parser.parse(Scanner.scan("(ab)")) == Sequence(Ch('a'), Ch('b')))
+    assert(Parser.parse(Scanner.scan("(a1)|(b2)")) == Alt(Sequence(Ch('a'), Ch('1')),
+                                                          Sequence(Ch('b'), Ch('2'))))
+    assert(Parser.parse(Scanner.scan("(a1|b)2")) == Sequence(Alt(Sequence(Ch('a'), Ch('1')), Ch('b')),
+                                                                          Ch('2')))
+    assert(Parser.parse(Scanner.scan("a(1|b)2")) == Sequence(Ch('a'), Alt(Ch('1'), Ch('b')), Ch('2')))
+    assert(Parser.parse(Scanner.scan("a(1|b2)")) == Sequence(Ch('a'), Alt(Ch('1'), Sequence(Ch('b'), Ch('2')))))
+  }
+
+  "Closure" should "bind correctly" in {
+    assert(Parser.parse(Scanner.scan("a*")) == Star(Ch('a')))
+    assert(Parser.parse(Scanner.scan("ab*")) == Sequence(Ch('a'), Star(Ch('b'))))
+    assert(Parser.parse(Scanner.scan("a(1|b)*2")) == Sequence(Ch('a'), Star(Alt(Ch('1'), Ch('b'))), Ch('2')))
+  }
+
+  "Non matching brackets" should "throw an error" in {
+    assertThrows[Error](Parser.parse(Scanner.scan("(()")))
+    assertThrows[Error](Parser.parse(Scanner.scan("((a)")))
+    assertThrows[Error](Parser.parse(Scanner.scan("((a)b*")))
+  }
+
+  "An alternation without two arguments" should "throw an error" in {
+    assertThrows[Error](Parser.parse(Scanner.scan("a|")))
+    assertThrows[Error](Parser.parse(Scanner.scan("|b")))
+    assertThrows[Error](Parser.parse(Scanner.scan("ab*|")))
+    assertThrows[Error](Parser.parse(Scanner.scan("|(ab*)")))
+  }
+
+  "Printing and parsing" should "be inverses of one another" in {
+    List(
+      "a",
+      "b",
+      "a|b",
+      "a|b*",
+      "(a|b)*",
+      "abc*"
+    ).foreach(regex => parsePrintInverse(regex))
+  }
+}
---
+++ src/test/scala/ScannerTest.scala
@@ -0,0 +1,43 @@
+class ScanTest
+
+import org.scalatest.flatspec.AnyFlatSpec
+import lexer.{Scanner, TAlternation, TChar, TClosure, TLeftBracket, TRightBracket}
+
+
+class ScannerFlatSpec extends AnyFlatSpec {
+  "Empty regex" should "produce an empty list of tokens" in {
+    assert(Scanner.scan("") == List())
+  }
+
+  "An character" should "produce a character token" in {
+    assert(Scanner.scan("a") == List(TChar('a')))
+    assert(Scanner.scan("8") == List(TChar('8')))
+  }
+
+  "Brackets" should "produce bracket tokens" in {
+    assert(Scanner.scan("(") == List(TLeftBracket))
+    assert(Scanner.scan(")") == List(TRightBracket))
+    assert(Scanner.scan("()") == List(TLeftBracket, TRightBracket))
+    assert(Scanner.scan("((") == List(TLeftBracket, TLeftBracket))
+  }
+
+  "An alternation" should "return an alternation token" in {
+    assert(Scanner.scan("|") == List(TAlternation))
+    assert(Scanner.scan("||") == List(TAlternation, TAlternation))
+  }
+
+  "A closure" should "return a closure token" in {
+    assert(Scanner.scan("*") == List(TClosure))
+    assert(Scanner.scan("**") == List(TClosure, TClosure))
+  }
+
+  "Consecutive tokens" should "be converted correctly" in {
+    assert(Scanner.scan("a*b") == List(TChar('a'), TClosure, TChar('b')))
+    assert(Scanner.scan("(a)*b") == List(TLeftBracket, TChar('a'), TRightBracket, TClosure, TChar('b')))
+  }
+
+  "White space" should "be ignored" in {
+    assert(Scanner.scan(" ") == List())
+    assert(Scanner.scan("a b") == List(TChar('a'), TChar('b')))
+  }
+}

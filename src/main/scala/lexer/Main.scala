bbc40297d9487587c562a0364b9f623e13167cce
James Lee-Jones <james@lee-jones.co.uk>
2025-04-14 17:07:56 +0100

    remove main

--- src/main/scala/lexer/Main.scala
+++ src/main/scala/lexer/Main.scala
@@ -1,12 +1,4 @@
 package lexer
 
 @main
-def main(): Unit = {
-  for {
-    result <- Parser.parseRegex(Scanner.scan("a(b|c)*"))
-  } do {
-    val dfa = nfaToDfa(regexToNfa(result))
-    dfa.printTransitions()
-    println(dfa.nodeAndTransitionCount())
-  }
-}
+def main(): Unit = {}

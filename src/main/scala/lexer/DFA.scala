1610471bf6f8721a147b263a694f7420f189912c
James Lee-Jones <36175797+JamesLee-Jones@users.noreply.github.com>
2025-04-14 17:09:38 +0100

    feat: convert NFA to DFA
    Convert an NFA to a DFA using the subset construction. All duplicated
    code between NFAs and DFAs is refactored into a common Automata trait.
    
    Fixes: #6.

---
+++ src/main/scala/lexer/DFA.scala
@@ -0,0 +1,121 @@
+package lexer
+
+import scala.collection.mutable
+
+/** A state in the DFA graph.
+  *
+  * @param transitions
+  *   the outgoing transitions from the current state.
+  */
+class DFAState(var transitions: Map[Option[Char], Id[DFAState]] = Map.empty)
+    extends AutomataState[DFAState, Id] {
+
+  /** Add a transition from the current state to a new state.
+    *
+    * @param input
+    *   The possibly empty character associated with the transition. None
+    *   represents an epsilon transition.
+    * @param state
+    *   The state to transition to.
+    */
+  def addTransition(maybeChar: Option[Char], state: DFAState): Unit = {
+    transitions += (maybeChar -> Id(state))
+  }
+
+}
+
+/** A representation of an DFA.
+  * @param initial
+  *   The initial state.
+  * @param accept
+  *   The set of accepting states of the DFA.
+  */
+case class DFA(initial: DFAState, var accept: Set[DFAState])
+    extends Automata[DFAState, Id]
+
+/** Get all states reachable from the current set of states by zero or more
+  * epsilon transitions.
+  * @param states
+  *   Initial states.
+  * @return
+  *   All states reachable from the current set of states by zero or more
+  *   epsilon transitions.
+  */
+def epsilonClosure(states: Set[NFAState]): Set[NFAState] = {
+  var result = states
+  // Compute the epsilon closure of every state reachable by an epsilon transition
+  // and accumulate the result
+  states.foreach(state =>
+    if state.transitions.contains(epsilon) then
+      result = result.union(epsilonClosure(state.transitions(epsilon)))
+  )
+
+  result
+}
+
+/** Convert an NFA to a DFA using the subset construction.
+  * @param nfa
+  *   The NFA to be converted.
+  * @return
+  *   A DFA which is equivalent to nfa.
+  */
+def nfaToDfa(nfa: NFA): DFA = {
+  // A map from the sets of states seen so far to their representative DFA state.
+  val visited = mutable.Map[Set[Int], DFAState]()
+
+  // Declare the starting DFA state and set of accepting states.
+  val dfaInitial = DFAState()
+  val dfaAccepting = mutable.Set[DFAState]()
+
+  // Compute the epsilon closure of the initial NFA state marking q0 as visited.
+  val q0 = epsilonClosure(Set(nfa.initial))
+  visited += (q0.map(state => state.id) -> dfaInitial)
+
+  // Create a workQueue for traversing the NFA
+  val workQueue = mutable.Queue[(Set[NFAState], DFAState)]()
+  workQueue.enqueue((q0, dfaInitial))
+
+  while (workQueue.nonEmpty) {
+    // Get the next element form the workQueue
+    val (q, representative) = workQueue.dequeue()
+
+    // q is a set of states, each with their own transition map. Combine these transition maps
+    // so that each key in the combined map maps to the union of the set of states in each individual
+    // transition map.
+    var combined: Map[Option[Char], Set[NFAState]] =
+      q.foldLeft(Map.empty)((map: Map[Option[Char], Set[NFAState]], state) =>
+        unionWith(
+          (m1: Set[NFAState], m2: Set[NFAState]) => m1.union(m2),
+          map,
+          state.transitions
+        )
+      )
+
+    // Don't consider epsilon transitions
+    combined = combined.removed(epsilon)
+
+    combined.foreach((char, states) => {
+      // Compute the epsilon closure of the states reachable from the current transition.
+      val reachable = epsilonClosure(states)
+      val reachableIdSet = reachable.map(state => state.id)
+
+      if (!visited.contains(reachableIdSet)) {
+        // If this set of states hasn't been visited before, create a new representative
+        // for these states and add a transition from the last representative to the new one.
+        val newRepresentative = DFAState()
+        representative.addTransition(char, newRepresentative)
+        visited += (reachableIdSet -> newRepresentative)
+        workQueue.enqueue((reachable, newRepresentative))
+      } else {
+        // If the set of states has been visited before, add a transition to the existing representative
+        // to show that it can be reached from the current state.
+        representative.addTransition(
+          char,
+          visited(reachable.map(state => state.id))
+        )
+      }
+    })
+  }
+
+  DFA(dfaInitial, dfaAccepting.toSet)
+}
--- src/main/scala/lexer/Main.scala
+++ src/main/scala/lexer/Main.scala
@@ -1,13 +1,4 @@
 package lexer
 
 @main
-def main(): Unit = {
-  for {
-    result <- Parser.parseRegex(Scanner.scan("a|a"))
-  } do {
-    val nfa = regexToNfa(Alt(Ch('a'), Ch('a')))
-    println(nfa)
-    println(nfa.printTransitions())
-    println(nfa.nodeAndTransitionCount())
-  }
-}
+def main(): Unit = {}
